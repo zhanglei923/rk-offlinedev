addaea15aaa264ae000890bdcb518b0e5f5f7c7a
zhanglei <zhanglei@xiaoshouyi.com>
2018-01-30 19:19:18 +0800

    update

--- offlinedev/jsmodule/processConfigRequest.js
+++ offlinedev/jsmodule/processConfigRequest.js
@@ -1,6 +1,9 @@
 var fs = require('fs');
 var pathutil = require('path');
 var requester = require('sync-request');
+var http = require('http');
+var unzip = require("unzip");
+var getConfig = require('./getConfig')
 
 var configJson = require('./getConfig').get()
 
@@ -24,11 +27,19 @@ module.exports = {
             content.forEach(function(fpath){
                 console.log(fpath)
             })
-            return content;
+            getConfig.initFiles()
 
+            var filename = caseName+".zip";
+            var filepath = pathutil.resolve(__dirname, "../mocking/"+filename)
+            var file = fs.createWriteStream(filepath);
+            var request = http.get("http://10.10.0.115/public/offlinedev/mocking/"+ filename, function(response) {
+                response.pipe(file);
+                setTimeout(function(){
+                     console.log('unzip', filepath)
+                    fs.createReadStream(filepath).pipe(unzip.Extract({ path: pathutil.resolve(__dirname, "../mocking/") }));
+                }, 4000)
+            });
         }
-
-        
     }
 }
 var getUrlContent = function(url){    
