d575b3cb9f3a980fa761fe6b14f57a9325f1ed4a
zhanglei <zhanglei@xiaoshouyi.com>
2018-01-30 16:58:34 +0800

    update

--- offlinedev/jsmodule/processConfigRequest.js
+++ offlinedev/jsmodule/processConfigRequest.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 var pathutil = require('path');
+var requester = require('sync-request');
 
 var configJson = require('./getConfig').get()
 
@@ -12,7 +13,28 @@ module.exports = {
             return 'done'
 
         }
+        if(/^\/offlinedev\/syncCases/.test(req.url)){
+            var caseName = req.body.caseName
+            console.log(caseName)
+            var url = 'http://10.10.0.115:3004/offlinedev/allfiles?casename=' + caseName
+            console.log(url)
+            var content = getUrlContent(url);
+            content = JSON.parse(content)
+            //console.log
+            content.forEach(function(fpath){
+                console.log(fpath)
+            })
+            return content;
+
+        }
 
         
     }
 }
+var getUrlContent = function(url){    
+        var furl = url
+        var data = requester('GET', furl, {timeout: 2000});   
+        console.log(furl) 
+        var webresultText = data.statusCode === 200 ? data.getBody().toString() : null;
+        return webresultText;
+};
