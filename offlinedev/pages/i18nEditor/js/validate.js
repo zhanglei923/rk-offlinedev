a795fe87edabdaf8592410d8b7844d2d99588951
zhanglei <zhanglei@xiaoshouyi.com>
2018-11-21 03:03:29 +0800

    up

--- offlinedev/pages/i18nEditor/js/validate.js
+++ offlinedev/pages/i18nEditor/js/validate.js
@@ -22,33 +22,36 @@ var do_validateValue = (key, value)=>{
 
 do_selfTest = () =>{
     $('#selfTestBtn').removeClass('wrong').removeClass('correct').html('testing...')
-    let ipt = $('#selftestInput')//document.getElementById('selftestInput')
-    let osuperJson = JSON.parse(JSON.stringify(OriginSuperJson));
-
     let errors = []
     let count = 0;
-    let _test = (key, val0, val)=>{
-        ipt.val(escapeValue(val));
-        val = unescapeValue(ipt.val());
-        if(typeof val0 !== 'undefined' && val0 !== val) {
-            errors.push({key, val0, val1: val})
+    let ids = ['selftestInput','selftestInput2'];
+    ids.forEach((id)=>{
+        let ipt = $('#'+id)//document.getElementById('selftestInput')
+        let osuperJson = JSON.parse(JSON.stringify(OriginSuperJson));
+    
+        let _test = (key, val0, val)=>{
+            ipt.val(escapeValue(val));
+            val = unescapeValue(ipt.val());
+            if(typeof val0 !== 'undefined' && val0 !== val) {
+                errors.push({key, val0, val1: val})
+            }
+            count++;
         }
-        count++;
-    }
-    let baddata1 = '~!@#$%^&*()_+=-0`|}{[]\":;?><$for,./'+"'\n "
-    baddata1 += baddata1
-    baddata1 += baddata1
-    _test('baddata1', baddata1, baddata1);//terrible data test
-    for(let key in osuperJson){
-        let val = osuperJson[key].en;
-        let val0 = val;
-        _test(key, val0, val)
-    }
-    for(let key in osuperJson){
-        let val = osuperJson[key].cn;
-        let val0 = val;
-        _test(key, val0, val)
-    }
+        let baddata1 = '~!@#$%^&*()_+=-0`|}{[]\":;?><$for,./'+"'\n "
+        baddata1 += baddata1
+        baddata1 += baddata1
+        _test('baddata1', baddata1, baddata1);//terrible data test
+        for(let key in osuperJson){
+            let val = osuperJson[key].en;
+            let val0 = val;
+            _test(key, val0, val)
+        }
+        for(let key in osuperJson){
+            let val = osuperJson[key].cn;
+            let val0 = val;
+            _test(key, val0, val)
+        }
+    })
     $('#selfTestBtn').addClass(errors.length > 0 ? 'wrong':'correct')
     $('#selfTestBtn').html(errors.length > 0 ? `${errors.length} Errors, Self-Test Failed!!`:`${count}safe`)
     if(errors.length > 0){
