1ae7aa806b84073d5075495933976657defa79e4
zhanglei <zhanglei@xiaoshouyi.com>
2018-11-20 20:03:46 +0800

    up

--- offlinedev/pages/i18nEditor/js/index.js
+++ offlinedev/pages/i18nEditor/js/index.js
@@ -8,7 +8,7 @@ var htmlEscape = (s) => {
         .replace(/>/g, '&gt;')
         .replace(/'/g, '&#039;')
         .replace(/"/g, '&quot;')
-        .replace(/\s/g, '&nbsp;')
+        //.replace(/\s/g, '&nbsp;')
         .replace(/\n/g, '<span class="keyofenter_mark">\\n</span>');
     w = w//.replace(/\{\d{1,}\}/g,(w)=>{return '<span class="unknown_mark">'+w+'</span>';})
         .replace(/\{\w{1,}\}/g,(w)=>{
