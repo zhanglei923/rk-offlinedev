4a7d9c54e473ed30c876fc84b27229879d963248
zhanglei <zhanglei@xiaoshouyi.com>
2018-11-22 02:37:02 +0800

    up

--- offlinedev/jsmodule/i18n/i18nValidator.js
+++ offlinedev/jsmodule/i18n/i18nValidator.js
@@ -1,9 +1,12 @@
 var fs = require('fs');
 var pathutil = require('path');
-var i18nUtil = require('../../../../apps-ingage-web/.cicd/i18n/i18nUtil')
-
+var i18nUtil;
+if(fs.existsSync(pathutil.resolve(__dirname, '../../../../apps-ingage-web/.cicd/i18n/i18nUtil.js'))){
+    i18nUtil = require('../../../../apps-ingage-web/.cicd/i18n/i18nUtil')
+}
 module.exports = {
     getReports: function(){
+        if(!i18nUtil)return;
         let untranslatedList = i18nUtil.loadUntranslateds();
         let allcn = i18nUtil.loadLanguageOfAll('all_zh-cn');
 
