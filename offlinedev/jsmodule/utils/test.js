bb61ca8e7f0c8c0f2b1d0795f66c3513dd134fc2
zhanglei <zhang.lei.923@gmail.com>
2019-01-14 11:50:32 +0800

    up

---
+++ offlinedev/jsmodule/utils/test.js
@@ -0,0 +1,18 @@
+let urlUtil = require('./url');
+
+console.log(urlUtil.getSaveName('/json/save.action?name=zhang&age=30'))
+console.log(urlUtil.getSaveName('/json/save/'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
