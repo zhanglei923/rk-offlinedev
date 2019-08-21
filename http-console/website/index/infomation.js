635e14a951fe91ee1505d6a9f8d1f06494535252
zhanglei <zhanglei@xiaoshouyi.com>
2019-08-21 20:11:53 +0800

    up

--- http-console/website/index/infomation.js
+++ http-console/website/index/infomation.js
@@ -59,8 +59,8 @@ let showSubProjects = (result)=>{
                         </span>
                         &nbsp;
                         (${item.branchname?item.branchname:'<span class="status_negative">不是工程！</span>'})
-                        ${branchMatch?'':`(<span class="status_negative">分支不符合要求</span>)`}
-                        ${branchMatch?'':`(<span class="status_negative">期望分支是${item.def_branchname}</span>)`}
+                        ${branchMatch?'':`(<span class="status_negative">分支不对</span>)`}
+                        ${branchMatch?'':`(<span class="status_negative">期望分支为：${item.def_branchname}</span>)`}
                         </td>
                     </tr>`
         })
