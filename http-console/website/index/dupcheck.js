0961f48366775e48980786d1e701f5781310a56b
zhanglei <zhanglei@xiaoshouyi.com>
2019-08-23 15:04:28 +0800

    up

--- http-console/website/index/dupcheck.js
+++ http-console/website/index/dupcheck.js
@@ -9,15 +9,16 @@ let showDupCheck = ()=>{
             console.log(result)
             let html = '正常'
             if(result.dupfiles && result.dupfiles.length > 0){
-                html = '<div class="status_negative"><div>以下文件同时存在于多个工程中</div>'
+                html = '<div><div>以下文件同时存在于多个工程中</div>'
                 result.dupfiles.forEach((dup)=>{
                     // {root1: "E:\workspaceGerrit\_sub_separation_test\apps-ingage-web\src\main\webapp", 
                     // root2: "E:\workspaceGerrit\_sub_separation_test\xsy-static-breeze", 
                     // relativepath: "embeded\breeze\breeze.lib.min.css"}
-                    html += `<div>${dup.root1}</div>
-                             <div>${dup.root2}</div>
-                             <div>${dup.relativepath}
-                            </div>`;
+                    html += `
+                             <div class="status_negative_fill" style="padding-left:10px;">${dup.relativepath}:</div>
+                             <div class="status_negative" style="padding-left:15px;">${dup.root1}</div>
+                             <div class="status_negative" style="padding-left:15px;">${dup.root2}</div>
+                            `;
                 })
                 html += '</div>'
             }
