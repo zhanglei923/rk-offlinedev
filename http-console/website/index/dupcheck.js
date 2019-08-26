b7251f1286e844d28927db76a4fff16465cb6240
zhanglei <zhanglei@xiaoshouyi.com>
2019-08-26 12:28:04 +0800

    up

--- http-console/website/index/dupcheck.js
+++ http-console/website/index/dupcheck.js
@@ -14,10 +14,9 @@ let showDupCheck = ()=>{
                     // root2: "E:\workspaceGerrit\_sub_separation_test\xsy-static-breeze", 
                     // relativepath: "embeded\breeze\breeze.lib.min.css"}
                     html += `
-                             <div class="status_negative_fill" style="padding-left:10px;">${dup.relativepath}</div>
-                             <div class="status_negative" style="padding-left:15px;">${dup.root1}</div>
-                             <div class="status_negative" style="padding-left:15px;">${dup.root2}</div>
-                            `;
+                             <div class="status_negative" style="padding-left:15px;">${dup.root1}<span style="text-decoration:underline;font-weight:bolder;">/${dup.relativepath}</span></div>
+                             <div class="status_negative" style="padding-left:15px;margin-bottom:7px;">${dup.root2}<span style="text-decoration:underline;font-weight:bolder;">/${dup.relativepath}</span></div>
+                             `;
                 })
                 html += '</div>'
             }
