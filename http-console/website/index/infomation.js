6e13b13038b7e50c910e80d18657d100451cf065
zhanglei <zhanglei@xiaoshouyi.com>
2019-08-29 14:55:17 +0800

    up

--- http-console/website/index/infomation.js
+++ http-console/website/index/infomation.js
@@ -65,7 +65,7 @@ let showSubProjects = (result)=>{
                             ${item.projectPath}
                         </span>
                         ${!item.def_branchname?'<span class="status_negative_fill">没有指定分支</span>':''}
-                        ${!item.projectExist?'<button class="clone_project_btn">立刻下载</button>&nbsp;<span class="status_negative_fill">目录不存在</span>':''}
+                        ${!item.projectExist?'<button class="clone_project_btn" style="display:none;">立刻下载</button>&nbsp;<span class="status_negative_fill">目录不存在</span>':''}
                         ${item.branchname?`<span class="${branchMatch?'status_positive_fill':'status_negative_fill'}">${item.branchname}</span>`:'<span class="status_negative_fill">不是git工程</span>'}
                         ${item.projectExist?`<button class="terminal_btn" onclick="openTerminal('${encodeURIComponent(item.projectPath)}')" ppath="${item.projectPath}">&gt;_</button>`:''}
                         ${branchMatch?'':`<span class="status_negative_fill">期望分支为：${item.def_branchname}</span>`}
