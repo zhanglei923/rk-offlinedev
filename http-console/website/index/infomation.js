0643e48fb70905c5bde210b755acf2aff66943ce
zhanglei <zhanglei@xiaoshouyi.com>
2019-10-11 17:33:40 +0800

    up

--- http-console/website/index/infomation.js
+++ http-console/website/index/infomation.js
@@ -33,7 +33,7 @@ let showInfomation = (result)=>{
         </tr>
         <tr><td align="right" valign="top">子工程：</td><td><table><tbody id="subproject_list"></tbody></table></td></tr>
         <tr><td align="right">转义ES6：</td><td>${userConfig.es6.autoTransformJs?'<span class="status_warn_fill">On</span>':'<span class="status_positive">Off</span>'}</td></tr>
-        <tr><td align="right" valign="top">子工程冲突：</td><td><table><tbody><tr><td><div id="subproject_verify" class="subproject_verify"><span class="status_warn">Verifying...</span></div></td></tr></tbody></table></td></tr>
+        <tr><td align="right" valign="top">子工程冲突：</td><td><table><tbody><tr><td><div id="subproject_verify" class="subproject_verify"><span class="status_loading">Verifying...</span></div></td></tr></tbody></table></td></tr>
         <tr>
             <td align="right" valign="top">Deploy目录：</td>
             <td>
@@ -61,42 +61,8 @@ let showInfomation = (result)=>{
     </table>
     `
     $('#infomation').html(html)
-    showGitStatus();
     showSubProjects(result)
-}
-let showGitStatus = ()=>{
-    $('span[git_project_info="true"]').each((i, span)=>{
-        span = $(span);
-        span.html('Loading...')
-        let encodedpath = span.attr('git_path')
-        let gitpath = decodeURIComponent(encodedpath)
-        //console.log(span, i)
-        $.ajax({
-            url: '/offlinedev/api/getGitInfo/',
-            cache: false,
-            method: 'POST',
-            data: {projectpath:encodeURIComponent(gitpath)},
-            success: function( response ) {
-                console.log(response.result)
-                let result = response.result;
-                let status = result.status;
-                let isClean = (status.ahead===0&&status.dirty===0&&status.stashes===0&&status.untracked===0)
-                let txt = 'git-dirty'
-                if(status.ahead===1) txt += ', git-need-sync'
-                let isDirty = !isClean;
-                let html = `<span class="${isDirty?'status_warn_fill':'status_positive_fill'}">${status.branch}</span>
-                            ${isDirty?`<span class="status_warn">${txt}</span>`:'<span class="status_positive"></span>'}
-                            <button class="terminal_btn" onclick="openTerminal('${encodeURIComponent(gitpath)}')" ppath="${gitpath}">&gt;_</button>
-                            `;
-                span.html(html);   
-                span.removeClass('status_warn') 
-            },
-            error:function(ajaxObj,msg,err){
-                span.html(`<span class="status_negative_fill">Load-Git-Info-Failed!${err}</span>`)
-            }
-        });
-                                            
-    })
+    showGitStatus();
 }
 let showSubProjects = (result)=>{
     let has = false;
@@ -126,44 +92,56 @@ let showSubProjects = (result)=>{
                         </span>
                         </td>
                         <td>
-                        ${item.def_branchname?`<span class="status_positive_fill">${item.def_branchname}</span>`:'<span class="status_negative_fill">没有指定分支</span>'}
+                        <span git_project_info="true" git_path="${encodeURIComponent(item.projectPath)}"></span>
+                        ${item.def_branchname?``:'<span class="status_negative_fill">没有指定分支</span>'}
                         ${!item.projectExist?'<button class="clone_project_btn" style="display:none;">立刻下载</button>&nbsp;<span class="status_negative_fill">目录不存在</span>':''}
-                        ${item.branchname?`${branchMatch?`<span class="status_positive_fill">=${item.branchname}</span>`:`<span class="status_negative_fill">!=${item.branchname}</span>`}&nbsp;<span id="gitdirty_${item.project}" class="status_warn">checking..</span>`:'<span class="status_negative_fill">不是git工程</span>'}
+                        ${item.branchname?
+                            `${branchMatch?
+                                `<span class="status_positive">match-define</span>`:
+                                `<span class="status_negative_fill">!=${item.branchname}</span>`}`:
+                            '<span class="status_negative_fill">不是git工程</span>'}
                         ${branchMatch?'':`<span class="status_negative_fill">期望分支为：${item.def_branchname}</span>`}
-                        ${item.projectExist?`<button class="terminal_btn" onclick="openTerminal('${encodeURIComponent(item.projectPath)}')" ppath="${item.projectPath}">&gt;_</button>`:''}
                         </td>
                     </tr>`
         })
-        showSubProjectGitStatus(result.projects)
+        //showSubProjectGitStatus(result.projects)
     }
     if(!has) html = `<tr><td align="right">无</td></tr>`
     $('#subproject_list').html(html)
 }
-let showSubProjectGitStatus = (projects)=>{
-    projects.forEach((item)=>{
+let showGitStatus = ()=>{
+    let loadingCss = 'status_loading'
+    $('span[git_project_info="true"]').each((i, span)=>{
+        span = $(span);
+        span.addClass(loadingCss)
+        span.html('Loading...')
+        let encodedpath = span.attr('git_path')
+        let gitpath = decodeURIComponent(encodedpath)
+        //console.log(span, i)
         $.ajax({
-            url: '/offlinedev/api/self_check/isGitDirty/',
+            url: '/offlinedev/api/getGitInfo/',
             cache: false,
             method: 'POST',
-            data: {
-                prjpath: encodeURIComponent(item.projectPath),
-                prjname: item.project
-            },
+            data: {projectpath:encodeURIComponent(gitpath)},
             success: function( response ) {
+                console.log(response.result)
                 let result = response.result;
-                // {"status":0,"result":{"dirty":false,"prjname":"xsy-static-breeze"}}
-                let div = $('#gitdirty_' + result.prjname);
-                if(result.dirty !== null){
-                    div.addClass(result.dirty?'status_warn_fill':'status_positive_fill')
-                    div.html(result.dirty?'有未提交代码':'git干净')
-                    result.dirty ? div.show() : div.hide()
-                }else{                    
-                    div.hide()
-                }
-                console.warn(result)
+                let status = result.status;
+                let isClean = (status.ahead===0&&status.dirty===0&&status.stashes===0&&status.untracked===0)
+                let txt = 'git-dirty'
+                if(status.ahead===1) txt += ', git-need-sync'
+                let isDirty = !isClean;
+                let html = `<span class="${isDirty?'status_warn_fill':'status_positive_fill'}">${status.branch}</span>
+                            ${isDirty?`<span class="status_warn">${txt}</span>`:'<span class="status_positive"></span>'}
+                            <button class="terminal_btn" onclick="openTerminal('${encodeURIComponent(gitpath)}')" ppath="${gitpath}">&gt;_</button>
+                            `;
+                span.html(html);   
+                span.removeClass(loadingCss) 
             },
             error:function(ajaxObj,msg,err){
+                span.html(`<span class="status_negative_fill">Load-Git-Info-Failed!${err}</span>`)
             }
         });
-    });
+                                            
+    })
 }
