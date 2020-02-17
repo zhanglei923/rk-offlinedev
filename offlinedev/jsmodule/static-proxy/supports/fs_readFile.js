3255c94d66134d28715b71901d69ab3358cfc6c6
zhanglei <zhanglei@xiaoshouyi.com>
2020-02-17 17:58:49 +0800

    up

--- offlinedev/jsmodule/static-proxy/supports/fs_readFile.js
+++ offlinedev/jsmodule/static-proxy/supports/fs_readFile.js
@@ -1,11 +1,28 @@
 let fs = require('fs-extra');
-let MemoCache = {};
+global.FileMemoCache = {};
 
 let fs_readFile = (fpath, opt, cb)=>{
     if(typeof cb === 'undefined') cb = ()=>{};
     if(!fs.existsSync(fpath)){
-        cb(null);
+        cb({
+            error: 'file not found'
+        }, null);
         return null;
+    }
+    let fstate = fs.lstatSync(fpath);
+    let ctime = fstate.ctimeMs;
+    let ctime36 = ctime.toString(36);
+    let mtime = fstate.mtimeMs;
+    let mtime36 = mtime.toString(36);
+    let cachekey = fpath;
+    //console.log(cachekey)
+    if(global.FileMemoCache[cachekey]){
+        let memo = global.FileMemoCache[cachekey];
+        if(memo.mtime36 === mtime36 && memo.ctime36 === ctime36){
+            //console.log('cc', global.FileMemoCache[cachekey])
+            cb(null, global.FileMemoCache[cachekey].content);
+            return global.FileMemoCache[cachekey].content;
+        }
     }
     let content;
     let err;
@@ -14,6 +31,11 @@ let fs_readFile = (fpath, opt, cb)=>{
     }catch(e){
         err = e;
     }
+    global.FileMemoCache[cachekey] = {
+        ctime36,
+        mtime36,
+        content
+    };
     cb(err, content);
     return content;
 }
