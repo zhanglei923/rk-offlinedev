5c5ca5257071146642622f0a716fe42843c52431
zhanglei <zhanglei@xiaoshouyi.com>
2020-02-18 05:45:33 +0800

    up

--- offlinedev/jsmodule/static-proxy/supports/fs_readFile.js
+++ offlinedev/jsmodule/static-proxy/supports/fs_readFile.js
@@ -1,8 +1,22 @@
 let fs = require('fs-extra');
+let pathutil = require('path')
 let configUtil = require('../../config/configUtil')
 
 global.FileMemoCache = {};
 
+let me = {}
+let getKey = (fullpath)=>{
+    return pathutil.normalize(fullpath).replace(/\/{1,}/g,',,,').replace(/\\{1,}/g,',,,');
+}
+let loadAllFiles = (rootpath)=>{
+
+}
+me.loadAllFiles = loadAllFiles;
+let canCache = (fullpath)=>{
+    //if(fullpath.match(/\.css$/)) return false;
+    return true;
+}
+me.canCache = canCache;
 let fs_readFile = (fpath, opt, cb)=>{
     if(typeof cb === 'undefined') cb = ()=>{};
     if(!fs.existsSync(fpath)){
@@ -14,11 +28,9 @@ let fs_readFile = (fpath, opt, cb)=>{
             cb({error: 'file stat error'}, null);
             return;
         }
-        let ctime = fstate.ctimeMs;
-        let ctime36 = ctime.toString(36);
-        let mtime = fstate.mtimeMs;
-        let mtime36 = mtime.toString(36);
-        let cachekey = fpath;
+        let ctime36 = fstate.ctimeMs.toString(36);
+        let mtime36 = fstate.mtimeMs.toString(36);
+        let cachekey = getKey(fpath);
         //console.log(cachekey)
         if(global.FileMemoCache[cachekey] && configUtil.getValue('debug.cacheStaticRequests')){
             let memo = global.FileMemoCache[cachekey];
@@ -31,6 +43,7 @@ let fs_readFile = (fpath, opt, cb)=>{
         // let content;
         // let read_err;
         fs.readFile(fpath, opt, (read_err, content)=>{
+            if(canCache(fpath))
             if(!read_err && configUtil.getValue('debug.cacheStaticRequests')){
                 global.FileMemoCache[cachekey] = {
                     ctime36,
@@ -42,7 +55,5 @@ let fs_readFile = (fpath, opt, cb)=>{
         });
     });
 }
-
-module.exports = {
-    fs_readFile
-}
+me.fs_readFile = fs_readFile;
+module.exports = me;
