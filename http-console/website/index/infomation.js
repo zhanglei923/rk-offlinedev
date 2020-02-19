51ec680b0d9509df2f0ea948df19b43348e7fff0
zhanglei <zhanglei@xiaoshouyi.com>
2020-02-19 18:51:33 +0800

    up

--- http-console/website/index/infomation.js
+++ http-console/website/index/infomation.js
@@ -38,11 +38,11 @@ let showInfomation = (result)=>{
             <button id="deepCheck_web_project" class="status_off_fill" onclick="javascript:deepCheck_web_project();">Web工程全面自检</button>
         </td></tr>
         <tr><td align="right" valign="top">功能：</td><td>
-            <span><span class="${userConfig.debug && userConfig.debug.gzip?'status_positive_fill':'status_off_fill'}">gzip</span>
-            <span><span class="${userConfig.debug && userConfig.debug.cacheStaticRequests?'status_positive_fill':'status_off_fill'}">自动缓存</span>
-            <span><span class="${userConfig.debug && userConfig.debug.concatStaticRequests?'status_positive_fill':'status_off_fill'}">聚合请求</span>
-            <span><span class="${userConfig.es6.autoTransformJs?'status_warn_fill':'status_off_fill'}">转义ES6</span>
-            <span><span class="${userConfig.debug && userConfig.debug.detect404RequireUrls?'status_dangous_fill':'status_off_fill'}">实时检测js</span>
+            <span><span class="${userConfig.debug && userConfig.debug.gzip?'status_is_on status_is_positive':'status_is_off status_is_negative'}">gzip</span>
+            <span><span class="${userConfig.debug && userConfig.debug.cacheStaticRequests?'status_is_on status_is_positive':'status_is_off status_is_negative'}">自动缓存</span>
+            <span><span class="${userConfig.debug && userConfig.debug.concatStaticRequests?'status_is_on status_is_positive':'status_is_off status_is_negative'}">聚合请求</span>
+            <span><span class="${userConfig.es6.autoTransformJs?'status_is_on status_is_warn':'status_is_off status_is_neutral'}">转义ES6</span>
+            <span><span class="${userConfig.debug && userConfig.debug.detect404RequireUrls?'status_is_on status_is_dangous':'status_is_off status_is_neutral'}">实时检测js</span>
         </td></tr>
         <tr><td align="right" valign="top">子工程：</td><td><table><tbody id="subproject_list"></tbody></table></td></tr>
         <tr><td align="right" valign="top">子工程冲突：</td><td><table><tbody><tr><td><div id="subproject_verify" class="subproject_verify"><span class="status_loading">Verifying...</span></div></td></tr></tbody></table></td></tr>
