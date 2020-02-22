a7f6f88998acbb2774be17c2481780fac48411b9
zhanglei <zhanglei@xiaoshouyi.com>
2020-02-22 17:04:46 +0800

    up

---
+++ scan/scan_parse-sea-config.js
@@ -0,0 +1,27 @@
+let fs = require('fs')
+const readline = require('readline');
+let pathutil = require('path')
+let _ = require('lodash')
+let rk = require('../offlinedev/jsmodule/utils/rk')
+let eachcontentjs = require('eachcontent-js')
+
+let sourcepath = `/Users/zhanglei/workspaces/apps-ingage-web/src/main/webapp/static/sea-config.js`
+let t0=new Date()*1;
+
+let parseSeajsConfig = (sourcepath)=>{
+    let content = fs.readFileSync(sourcepath,'utf8');
+    let arr = content.split('\n');
+    let keyvalue = []
+    let isIn = false;
+    arr.forEach((line)=>{
+        if(isIn && /\}/.test(line)) isIn = false;
+        if(isIn){
+            keyvalue.push(line)
+        }
+        if(/\balias\b\s{0,}\:/g.test(line)) isIn = true;
+
+    })
+    console.log(keyvalue)
+}
+parseSeajsConfig(sourcepath)
+console.log(new Date()*1-t0)
