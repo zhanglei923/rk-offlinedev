b49cd349765be5f187af26091d772a047b6a27cb
zhanglei <zhanglei@xiaoshouyi.com>
2020-02-23 19:52:31 +0800

    up

--- offlinedev/jsmodule/static-proxy/staticHotLoader.js
+++ offlinedev/jsmodule/static-proxy/staticHotLoader.js
@@ -1,14 +1,56 @@
 var fs = require('fs');
 var pathutil = require('path');
+var fs_readFile = require('./supports/fs_readFile')
+var getConfig = require('../config/configUtil')
 
-var configJson = require('../config/configUtil').get()
+let webFolder = getConfig.getWebRoot()
+let staticFolder = getConfig.getStaticFolder()
+let sourceFolder = getConfig.getSourceFolder()
+var webappFolder = getConfig.getWebAppFolder()
 
-let me = {
-    isHotUrl: (url)=>{
-        return /\_hotresponse\_\.js$/.test(url);
-    },
-    loadContent: (res, url)=>{
-        res.send(`hot-content`);
+let isHotUrl = (url)=>{
+    return /\_hotresponse\_\.js$/.test(url);
+}
+let loadContent = (res, url)=>{
+    // https://crm-dev61rs.ingageapp.com/static/source/products/bi/common/service/_hotresponse_.js
+    let dir = pathutil.parse(url).dir;
+    dir = dir.replace(/^\/{1,}/, './')
+    dir = pathutil.resolve(webappFolder, dir);
+    console.log('dir=', dir)
+    if(!fs.existsSync(dir)){
+        console.log(`[rk error]not found: ${dir}`)
+        res.sendStatus(404).send(`Not Found: ${dir}`)
+        return;
     }
+    let files = []
+    var list = fs.readdirSync(dir)
+    list.forEach(function(file) {
+        file = dir + '/' + file
+        //console.log(file)
+        var stat = fs.statSync(file)
+        if (stat && !stat.isDirectory()) {
+            files.push(file)
+        }
+    })
+    let len = files.length;
+    let fullcontent = ''
+    let onall = (fullcontent)=>{
+        res.send(fullcontent);
+    }
+    files.forEach((fullfilepath)=>{
+        fs_readFile.fs_readFile(fullfilepath, {encoding:'utf8'}, (err, jsContent, fileinfo) => {  
+            len--;
+            if(err){}else{
+                fullcontent += `;${fullfilepath};\n${jsContent}`
+            }
+            if(len === 0){
+                onall(fullcontent)
+            }
+        });
+    })
+}
+let me = {
+    isHotUrl,
+    loadContent
 }
 module.exports = me;
