19105038c35812acb53224786e10af3dc13e0b11
zhanglei <zhanglei@xiaoshouyi.com>
2020-02-23 12:55:17 +0800

    up

--- offlinedev/jsmodule/static-injects/seajs/seajs-text.js
+++ offlinedev/jsmodule/static-injects/seajs/seajs-text.js
@@ -210,7 +210,7 @@
   let initRkOfflineDev = function(){
     $(function(){
         $('body').append('<div style="position:fixed !important;background-color:#002520 !important;left:1px;font-size: 10px !important;bottom:1px;z-index:999998765 !important;">'+
-                          '<a style="color:#2fff56 !important;" href="http://localhost:666/offlinedev-http-console/" target="_blank">rk-offlinedev已激活</a>'+
+                          '<a style="color:#2fff56 !important;" href="http://localhost:666/offlinedev-http-console/" target="_blank">rk-offlinedev开启</a>'+
                         '</div>'
         );
         console.warn('[rk-offlinedev]seajs-text注入成功')
