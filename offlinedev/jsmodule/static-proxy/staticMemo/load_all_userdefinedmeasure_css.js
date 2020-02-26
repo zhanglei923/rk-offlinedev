931102b14264b57491a500d42166e4a8b66dd7e0
zhanglei <zhanglei@xiaoshouyi.com>
2020-02-26 21:26:50 +0800

    up

--- offlinedev/jsmodule/static-proxy/staticMemo/load_all_userdefinedmeasure_css.js
+++ offlinedev/jsmodule/static-proxy/staticMemo/load_all_userdefinedmeasure_css.js
@@ -7,18 +7,18 @@ let updateCss = require('../updators/updateCss')
 
 module.exports = {
     isMyHotUrl:(url)=>{
-        return /all\-products\_userdefinedmeasure\-widgets\_HOT\.css$/.test(url);
+        return /all\-userdefinedmeasure\_HOT\.css$/.test(url);
     },
     shouldReplacedWithThis:(sourceDir, req_realpath)=>{
         if(req_realpath.match(/\.css$/)){
             let bi_path = pathutil.resolve(sourceDir, './products/userdefinedmeasure')
             if(is_path_inside(req_realpath, bi_path)){
-                return 'products/userdefinedmeasure/all-products_userdefinedmeasure-widgets_HOT.css';
+                return 'products/userdefinedmeasure/all-userdefinedmeasure_HOT.css';
             }
         }
     },
     load:(webappFolder, callback)=>{
-        let destFile = pathutil.resolve(webappFolder, './static/source/platform/core/css/all-products_userdefinedmeasure-widgets_HOT.css')
+        let destFile = pathutil.resolve(webappFolder, './static/source/platform/core/css/all-userdefinedmeasure_HOT.css')
         updateCss.concatCss({
             sourceDirList: [
                 pathutil.resolve(webappFolder, './static/source/products/userdefinedmeasure')
