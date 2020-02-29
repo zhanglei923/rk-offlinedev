cc1f90e4a0241bc75ad50301a988c0109889f99d
zhanglei <zhanglei@xiaoshouyi.com>
2020-03-01 03:45:53 +0800

    up

--- offlinedev/jsmodule/static-proxy/supports/pathfinder.js
+++ offlinedev/jsmodule/static-proxy/supports/pathfinder.js
@@ -61,10 +61,10 @@ module.exports = {
         fullfilepath = prettifyFilePath(fullfilepath);
         //如果有些想被转接到rk-offlinedev本地的js文件，在这里变一下路径
         if(fullfilepath.match(/\/seajs\-text\.js$/)){
-            fullfilepath = pathutil.resolve(__filename, '../../../static-injects/seajs/seajs-text.js');
+            fullfilepath = pathutil.resolve(__filename, '../../../static-injects/files/seajs/seajs-text.js');
         }else 
         if(fullfilepath.match(/seajs\/sea\.js$/)){
-            fullfilepath = pathutil.resolve(__filename, '../../../static-injects/seajs/sea.js');
+            fullfilepath = pathutil.resolve(__filename, '../../../static-injects/files/seajs/sea.js');
         }
         fullfilepath = rk_formatPath(fullfilepath)
         return {
