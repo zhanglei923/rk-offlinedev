a4a874e5cfc9a55b01710b4b51d220ea64f1d23b
zhanglei <zhanglei@xiaoshouyi.com>
2020-03-02 05:42:32 +0800

    up

--- offlinedev/jsmodule/static-proxy/staticMemoLoader.js
+++ offlinedev/jsmodule/static-proxy/staticMemoLoader.js
@@ -9,6 +9,11 @@ let load_all_bi_widgets_css = require('./staticMemo/load_all_bi_widgets_css')
 let load_all_userdefinedmeasure_css = require('./staticMemo/load_all_userdefinedmeasure_css')
 let load_all_lib_css = require('./staticMemo/load_all_lib_css')
 
+let css_loaders = [
+    load_all_xsy_widgets_css,
+    load_all_bi_widgets_css,
+    load_all_userdefinedmeasure_css
+]
 
 let webFolder = getConfig.getWebRoot()
 let staticFolder = getConfig.getStaticFolder()
@@ -24,28 +29,15 @@ let isHotUrl = (url)=>{
     return false;
 }
 let loadHotCss = (res, url)=>{
-    if(load_all_xsy_widgets_css.isMyHotUrl(url)){
-        load_all_xsy_widgets_css.load(webappFolder, (content)=>{
-            res.send(content)
-        })
-        return;
-    }else if(load_all_bi_widgets_css.isMyHotUrl(url)){
-        load_all_bi_widgets_css.load(webappFolder, (content)=>{
-            res.send(content)
-        })
-        return;
-    }else if(load_all_userdefinedmeasure_css.isMyHotUrl(url)){
-        load_all_userdefinedmeasure_css.load(webappFolder, (content)=>{
-            res.send(content)
-        })
-        return;
+    for(let i=0;i<css_loaders.length;i++){
+        let loader = css_loaders[i];
+        if(loader.isMyHotUrl(url)){
+            loader.load(webappFolder, (content)=>{
+                res.send(content)
+            })
+            break;
+        }
     }
-    // else if(load_all_lib_css.isMyHotUrl(url)){
-    //     load_all_lib_css.load(webappFolder, (content)=>{
-    //         res.send(content)
-    //     })
-    //     return;
-    // }
 }
 let loadHotJs = (res, url)=>{
     if(load_hot_bundle_js.isMyHotUrl(url)){
