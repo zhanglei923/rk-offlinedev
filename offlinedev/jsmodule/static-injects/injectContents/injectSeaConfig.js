181ee0426c1f90082d4dc260a88b6a5d0027e9a8
zhanglei <zhanglei@xiaoshouyi.com>
2020-03-03 06:24:53 +0800

    up

--- offlinedev/jsmodule/static-injects/injectContents/injectSeaConfig.js
+++ offlinedev/jsmodule/static-injects/injectContents/injectSeaConfig.js
@@ -2,6 +2,7 @@
 let fs = require('fs');
 let pathutil = require('path');
 let eachcontentjs = require('eachcontent-js')
+let fs_readFile = require('../../static-proxy/supports/fs_readFile')
 var getConfig = require('../../config/configUtil')
 
 let isMe = (fpath)=>{ //src/main/webapp/static/router.js
@@ -13,42 +14,15 @@ let isMe = (fpath)=>{ //src/main/webapp/static/router.js
 let updateJs = (info, content)=>{        
     let fullfilepath = info.fullfilepath;
     if(!isMe(fullfilepath)) return content;
-    if(getConfig.getValue('debug.concat') !== 'concat') return content;
+    if(getConfig.getValue('debug.mode') !== 'concat') return content;
 
-    let script = `;
-            let sea_cfg = seajs.config();
-            sea_cfg.debug = false;
-            //sea_cfg.base = SESSION.resDomain + '/frontend/static/deploy/';
-            sea_cfg.base = SESSION.resDomain + '/static/deploy/';
-            var vMap = /*map start*/ {} /*map end*/ ;
-            var release_timestamp;
-            if(window.jsHashMap) {
-                vMap = window.jsHashMap;
-                cacheForAdmin(window.jsHashMap)
-                release_timestamp = window.jsHashMap.release_timestamp;
-            }
-            sea_cfg.map = [function(uri) {
-                var id = uri.split('/static/deploy/')[1];
-                var hashid = vMap[id];
-                if(true){
-                    var olduri = uri;
-                    uri = uri.replace(/\.\w+$/, function(a){
-                        return '.' + hashid + a
-                    })
-                    return vMap[id] ? 
-                        uri : 
-                        release_timestamp ? 
-                            olduri + '?' + release_timestamp : 
-                            olduri ;
-                }else{
-                    return vMap[id] ? [uri, vMap[id]].join('?') : uri;
-                }
-            }];
-            seajs.config(sea_cfg)
-            console.warn('[rk-offlinedev]更新了sea-config的逻辑');
-    `;
-    //console.log('[rk-offlinedev]更新了sea-config的逻辑')
-    content += script;
+    let dir = pathutil.parse(__filename).dir;
+    let jspath = pathutil.resolve(dir, './injectSeaConfig_script_content.js')
+    let jscontent;
+    fs_readFile.fs_readFile(jspath, {encoding:'utf8', be_sync: true}, (err, content, fileinfo) => {
+        jscontent = content;
+    });
+    content += jscontent;
     return content;
 }
 module.exports = {
