e605c462cb884f2801c77ebd1f762775f4c80a50
zhanglei <zhanglei@xiaoshouyi.com>
2020-03-02 12:51:50 +0800

    up

--- offlinedev/jsmodule/static-proxy/test/test_fs_readFile.js
+++ offlinedev/jsmodule/static-proxy/test/test_fs_readFile.js
@@ -1,12 +1,14 @@
 let fs_readFile = require('../supports/fs_readFile')
 
 let fpath = `/Users/zhanglei/workspaces/apps-ingage-web/src/main/webapp/static/source/core/i18n/untranslated.js`
+//默认走异步读取
 fs_readFile.fs_readFile(fpath, {encoding:'utf8'}, (err, content, fileinfo) => {
     console.log(content)
     console.log(`done1.1`)
 });
 console.log(`done1.2`)
 
+//走同步模式读取
 let content2;
 fs_readFile.fs_readFile(fpath, {encoding:'utf8', be_sync: true}, (err, content, fileinfo) => {
     content2 = content;
