c9ca3bff910c55ddcdc824cc1725d03a81b0c1b0
zhanglei <zhanglei@xiaoshouyi.com>
2020-03-03 08:19:58 +0800

    up

--- offlinedev/jsmodule/static-proxy/static-proxy.js
+++ offlinedev/jsmodule/static-proxy/static-proxy.js
@@ -4,15 +4,9 @@ let staticFilter = require('../static-filter/filter')
 var getConfig = require('../config/configUtil')
 var staticFileLoader = require('./staticFileLoader')
 let staticMemoLoader = require('./staticMemoLoader');
-let updateScript_1st = require('./updators/updateScript_1st')
-let updateScript_Tpl = require('./updators/updateScript_Tpl')
-let updateScript_i18n = require('./updators/updateScript_i18n')
-let updateScript_CmdConcat = require('./updators/updateScript_CmdConcat')
-let updateScript_CssUrl = require('./updators/updateScript_CssUrl')
+
 let scanner = require('../../codeScan/scan')
 
-let enableLevel2Cache = getConfig.getValue('debug.autoCacheStaticLevel2');
-let level2JsCache = {}//这里缓存处理过的js文本，如果内存消耗过高，建议关闭
 
 let linkToStaticFile = (req, res, next) => {
     let req_path = req.path;
@@ -91,24 +85,7 @@ let linkToStaticFile = (req, res, next) => {
                 if(info.fullfilepath)res.set('.rk-local-file', info.fullfilepath);
                 res.set('.rk-cached', is_rk_cached);
 
-                let newMC36 = info.fileinfo.mc36;
-                let level2needsupdate = (!level2JsCache[req_path] || (level2JsCache[req_path].mc36 !== newMC36))
-                if(!enableLevel2Cache || level2needsupdate){
-                    jscontent = updateScript_1st.updateJs(info, jscontent)
-                    //jscontent = updateScript_CmdConcat.updateJs(info, jscontent)
-                    jscontent = updateScript_CssUrl.updateJs(info, jscontent)
-    
-                    level2JsCache[req_path] = {
-                        mc36: newMC36,
-                        jscontent
-                    }
-                    //console.log('level2')
-                }else{
-                    jscontent = level2JsCache[req_path].jscontent;
-                }
-                //无论如何都要处理的
-                jscontent = updateScript_Tpl.updateJs(info, jscontent)
-                jscontent = updateScript_i18n.updateJs(info, jscontent)
+                jscontent = require('./updators/mode_source').updateSource(req_path, info, jscontent);
                 jscontent = require('../static-injects/injectContents/injectSeaConfig').updateJs(info, jscontent);
 
                 if(root) jscontent =//`//[rk][main]${root}\n`+ 
--- offlinedev/jsmodule/static-proxy/updators/mode_source.js
+++ offlinedev/jsmodule/static-proxy/updators/mode_source.js
@@ -1,5 +1,32 @@
-let updateSource = function (html){
+var getConfig = require('../../config/configUtil')
+let updateScript_1st = require('./updateScript_1st')
+let updateScript_Tpl = require('./updateScript_Tpl')
+let updateScript_i18n = require('./updateScript_i18n')
+let updateScript_CmdConcat = require('./updateScript_CmdConcat')
+let updateScript_CssUrl = require('./updateScript_CssUrl')
+let enableLevel2Cache = getConfig.getValue('debug.autoCacheStaticLevel2');
+let level2JsCache = {}//这里缓存处理过的js文本，如果内存消耗过高，建议关闭
 
+let updateSource = function (req_path, info, jscontent){
+    let newMC36 = info.fileinfo.mc36;
+    let level2needsupdate = (!level2JsCache[req_path] || (level2JsCache[req_path].mc36 !== newMC36))
+    if(!enableLevel2Cache || level2needsupdate){
+        jscontent = updateScript_1st.updateJs(info, jscontent)
+        //jscontent = updateScript_CmdConcat.updateJs(info, jscontent)
+        jscontent = updateScript_CssUrl.updateJs(info, jscontent)
+
+        level2JsCache[req_path] = {
+            mc36: newMC36,
+            jscontent
+        }
+        //console.log('level2')
+    }else{
+        jscontent = level2JsCache[req_path].jscontent;
+    }
+    //无论如何都要处理的
+    jscontent = updateScript_Tpl.updateJs(info, jscontent)
+    jscontent = updateScript_i18n.updateJs(info, jscontent)
+    return jscontent;
 }
 module.exports = {
     updateSource
