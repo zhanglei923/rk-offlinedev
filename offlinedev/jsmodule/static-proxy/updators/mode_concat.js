2d6ac59008f8dad689d69aa221f2f342a9bd006d
zhanglei <zhanglei@xiaoshouyi.com>
2020-03-03 14:16:18 +0800

    up

--- offlinedev/jsmodule/static-proxy/updators/mode_concat.js
+++ offlinedev/jsmodule/static-proxy/updators/mode_concat.js
@@ -4,14 +4,15 @@ let updateScript_Tpl = require('./updateScript_Tpl')
 let updateScript_i18n = require('./updateScript_i18n')
 let updateScript_cmd_HOT = require('./updateScript_cmd_HOT')
 let updateScript_CssUrl = require('./updateScript_CssUrl')
+let injectSeaConfig = require('../../static-injects/injectContents/injectSeaConfig')
 let enableLevel2Cache = getConfig.getValue('debug.autoCacheStaticLevel2');
 let level2JsCache = {}//这里缓存处理过的js文本，如果内存消耗过高，建议关闭
 
 let updateSource = function (req_path, info, jscontent){
     jscontent = updateScript_1st.updateJs(info, jscontent)
+    jscontent = injectSeaConfig.updateJs(info, jscontent);
     jscontent = updateScript_cmd_HOT.updateJs(info, jscontent)//必须在其他update之前，不然无法更改依赖数组里的值
     jscontent = updateScript_CssUrl.updateJs(info, jscontent)
-    jscontent = require('../../static-injects/injectContents/injectSeaConfig').updateJs(info, jscontent);
     jscontent = updateScript_i18n.updateJs(info, jscontent)
     return jscontent;
 }
