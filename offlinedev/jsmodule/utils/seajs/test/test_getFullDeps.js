29183f74c29215ae1e04857ade69231ff71fdcc7
zhanglei <zhanglei@xiaoshouyi.com>
2020-03-04 16:43:20 +0800

    up

--- offlinedev/jsmodule/utils/seajs/test/test_getFullDeps.js
+++ offlinedev/jsmodule/utils/seajs/test/test_getFullDeps.js
@@ -28,16 +28,24 @@ let expect= 'x,z,y,a,u,v,b,1,c,2,e,f,d,3,0,11,12'
 
 
 
-
+let hitedId = {}
+let _push = (array, pathid)=>{
+    if(!hitedId[pathid]){
+        hitedId[pathid] = true;
+        array.push(pathid)
+    }
+    return array;
+}
 let do_getAllDepsFiles = (deps, fulldeps,pathid)=>{
     //console.log(pid, pathid)
     let arr = deps[pathid];
     if(arr){
         arr.forEach((fpath)=>{
             do_getAllDepsFiles(deps, fulldeps, fpath);
-            fulldeps.push(fpath)
+            fulldeps = _push(fulldeps, fpath)
+            //fulldeps.push(fpath)
         })
-        fulldeps.push(pathid)
+        fulldeps = _push(fulldeps, pathid)
     }
 }
 let getAllDepsFiles = (deps, initId)=>{
@@ -45,7 +53,8 @@ let getAllDepsFiles = (deps, initId)=>{
     do_getAllDepsFiles(deps, fulldeps, initId)
 
     for(let pathid in deps){
-        fulldeps.push(pathid)
+        //fulldeps.push(pathid)
+        fulldeps = _push(fulldeps, pathid)
     }
     fulldeps = _.uniq(fulldeps)
     return fulldeps;
@@ -62,6 +71,24 @@ let thisdir = pathutil.parse(__filename).dir
 let json = fs.readFileSync(thisdir+'/dependencyMap.json');
 json = JSON.parse(json)
 
-let fullarr = getAllDepsFiles(json, "core/rkloader.js")
+let arr1 = getAllDepsFiles(json, "core/rkloader.js")
+arr1 = _.uniq(arr1)
+console.log(arr1.length)
+let arr2 = getAllDepsFiles(json, 'page/js/frame/pageMainCtrl.js')
+arr2 = _.uniq(arr2)
+console.log(arr2.length)
+let arr3 = getAllDepsFiles(json, 'oldcrm/js/core/common-crm.js')
+arr3 = _.uniq(arr3)
+console.log(arr3.length)
+
+fullarr = [];
+fullarr = fullarr.concat(arr1);fullarr = _.uniq(fullarr)
+fullarr = fullarr.concat(arr1);fullarr = _.uniq(fullarr)
+fullarr = fullarr.concat(arr1);fullarr = _.uniq(fullarr)
+
 fs.writeFileSync(thisdir+'/full_dependencylist.txt', fullarr.join('\n'))
+
+
+
+
 //console.log(json)
