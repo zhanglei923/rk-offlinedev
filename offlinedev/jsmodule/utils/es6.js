5a83818a972bae30c36d4399cbfde5736820a9f6
zhanglei <zhanglei@xiaoshouyi.com>
2020-03-09 12:23:36 +0800

    up

--- offlinedev/jsmodule/utils/es6.js
+++ offlinedev/jsmodule/utils/es6.js
@@ -1,32 +1,51 @@
 var babel = require("@babel/core");
+var UglifyJS = require("uglify-js");
 
-var _thisUtil = {
-    transform:(jsContent, opt)=>{
-        if(typeof opt === 'undefined') opt = {}
-        var script = jsContent+'';
-        try{
-            var result = babel.transform(script, {
-                plugins: [
-                    // "@babel/plugin-proposal-object-rest-spread",
-                    // "@babel/plugin-transform-arrow-functions"
-                ],
-                "presets": [//presents 是plugins的集合，npm里有其他定制的presets可用
-                    [
-                      "@babel/preset-env",
-                      {
-                        "useBuiltIns": false//"entry"
-                      }
-                    ]
-                ]
-              });
-            //console.log(result)
-            //console.log('map', result.map)
-            script = result.code;
-        }catch(e){
-            let fullfilepath = opt.fullfilepath;
-            console.log(`  Warn: ${fullfilepath} failed at transform es6:`, e)
-        }
-        return script;
+let minify = (jsContent, opt)=>{
+    if(typeof opt === 'undefined') opt = {
+        // mangle:{
+        //     reserved:['require' ,'exports' ,'module' ,'$']
+        // }
     }
+    jsContent = transform(jsContent);
+    var result = UglifyJS.minify(jsContent, opt);
+    var minContent = result.code;
+    if(result.error) {
+        console.log(result.error); 
+        return jsContent;
+    }else{
+        return minContent;
+    }
+}
+let transform = (jsContent, opt)=>{
+    if(typeof opt === 'undefined') opt = {}
+    var script = jsContent+'';
+    try{
+        var result = babel.transform(script, {
+            plugins: [
+                // "@babel/plugin-proposal-object-rest-spread",
+                // "@babel/plugin-transform-arrow-functions"
+            ],
+            "presets": [//presents 是plugins的集合，npm里有其他定制的presets可用
+                [
+                  "@babel/preset-env",
+                  {
+                    "useBuiltIns": false//"entry"
+                  }
+                ]
+            ]
+          });
+        //console.log(result)
+        //console.log('map', result.map)
+        script = result.code;
+    }catch(e){
+        let fullfilepath = opt.fullfilepath;
+        console.log(`  Warn: ${fullfilepath} failed at transform es6:`, e)
+    }
+    return script;
+}
+var _thisUtil = {
+    transform,
+    minify
 };
 module.exports = _thisUtil;
--- offlinedev/jsmodule/utils/test/test_es6.js
+++ offlinedev/jsmodule/utils/test/test_es6.js
@@ -11,3 +11,64 @@ console.log(code2)
 code = `sdfasdfas/sdfas323(&*(&^))`
 code2 = es6.transform(code);
 console.log(code2)
+
+code = `
+
+define(function(require, exports, module) {
+    'use strict';
+    $('#a');//a jquery
+    var babel = require("@babel/core");
+    var UglifyJS = require("uglify-js");
+
+    let minify = (jsContent, opt)=>{
+        if(typeof opt === 'undefined') opt = {}
+        jsContent = transform(jsContent);
+        var result = UglifyJS.minify(jsContent);
+        var minContent = result.code;
+        if(result.error) {
+            console.log(result.error); 
+            return jsContent;
+        }else{
+            return minContent;
+        }
+    }
+    let transform = (jsContent, opt)=>{
+        if(typeof opt === 'undefined') opt = {}
+        var script = jsContent+'';
+        try{
+            var result = babel.transform(script, {
+                plugins: [
+                    // "@babel/plugin-proposal-object-rest-spread",
+                    // "@babel/plugin-transform-arrow-functions"
+                ],
+                "presets": [//presents 是plugins的集合，npm里有其他定制的presets可用
+                    [
+                    "@babel/preset-env",
+                    {
+                        "useBuiltIns": false//"entry"
+                    }
+                    ]
+                ]
+            });
+            //console.log(result)
+            //console.log('map', result.map)
+            script = result.code;
+        }catch(e){
+            let fullfilepath = opt.fullfilepath;
+        }
+        return script;
+    }
+    var _thisUtil = {
+        transform,
+        minify
+    };
+    module.exports = _thisUtil;
+});
+
+`
+code2 = es6.minify(code, {        
+    mangle:{
+        reserved:['require' ,'exports' ,'module' ,'$']
+    }
+});
+console.log(code2)
