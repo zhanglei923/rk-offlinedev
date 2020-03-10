22de6bdff27134ab7b78aae8ada1fd972982d59a
zhanglei <zhanglei@xiaoshouyi.com>
2020-03-10 18:01:59 +0800

    up

--- offlinedev/jsmodule/static-injects/injectFiles/inject_global_script_Source.js
+++ offlinedev/jsmodule/static-injects/injectFiles/inject_global_script_Source.js
@@ -74,35 +74,3 @@ seajs.use = function(srcs, callback, a0,a1,a2,a3,a4,a5,a6,a7){
     return rk_offlinedev_Original_seause.call(seajs, srcs, fun2, a0,a1,a2,a3,a4,a5,a6,a7)
 }
 console.warn('[rk-offlinedev]已拦截seajs.use方法');
-
-
-//*********************************************************************************/
-//   rk-offlinedev 注入一些辅助性代码
-//*********************************************************************************/
-(function(){
-    var initRkOfflineDev = function(){
-        var div = document.createElement('div');
-        document.body.appendChild(div)
-        div.setAttribute('id','rk-offlinediv')
-        div.style.position='fixed';
-        div.style['background-color']='#002520';
-        div.style.color='#2fff56'
-        div.style['font-size']= '11px';
-        div.style.left='0px';
-        div.style.bottom='0px';
-        div.style.padding='0';
-        div.style['z-index']='999998765';
-  
-        let webbranch = rk_offlinedev.userConfig.webProjectInfo.branch;
-        let debugmode = rk_offlinedev.userConfig.debug.mode;
-        debugmode = debugmode?debugmode:'?'
-        div.innerHTML='<a style="color:#2fff56 !important;" href="http://localhost:666/offlinedev-http-console/" target="_blank">rk-offlinedev</a>,&nbsp;"'+debugmode+'",&nbsp;('+webbranch+')'
-        console.warn('[rk-offlinedev]seajs-text注入成功')
-    };
-    initRkOfflineDev()
-    // if(document.all) {   
-    //   window.attachEvent('onload', initRkOfflineDev);   
-    // }else{   
-    //   window.addEventListener('load', initRkOfflineDev, false);   
-    // }
-  })()
--- offlinedev/jsmodule/static-proxy/updators/updateScript_1st.js
+++ offlinedev/jsmodule/static-proxy/updators/updateScript_1st.js
@@ -11,6 +11,7 @@ let isFirstJs = (fpath)=>{
     return false;
 }
 let inject_global_script;
+let inject_global_mode_script;
 let updateJs = (info, content)=>{        
     let fullfilepath = info.fullfilepath;
     if(isFirstJs(fullfilepath)){
@@ -29,7 +30,11 @@ let updateJs = (info, content)=>{
         if(getConfig.getValue('debug.mode')==='source') srcpath = pathutil.resolve(dir, '../../static-injects/injectFiles/inject_global_script_Source.js');
         if(getConfig.getValue('debug.mode')==='concat') srcpath = pathutil.resolve(dir, '../../static-injects/injectFiles/inject_global_script_Concat.js');
         
-        if(!inject_global_script) inject_global_script = fs.readFileSync(srcpath, 'utf8')
+        if(!inject_global_mode_script) inject_global_mode_script = fs.readFileSync(srcpath, 'utf8')
+        defaultjs += inject_global_mode_script;
+
+        let gsrcpath = pathutil.resolve(dir, '../../static-injects/injectFiles/inject_global_script.js');
+        if(!inject_global_script) inject_global_script = fs.readFileSync(gsrcpath, 'utf8')
         defaultjs += inject_global_script;
 
         defaultjs += `\n//****** END *******//\n`
