8a029651251ac2bb9a173780be6182b9f87e80b6
zhanglei <zhanglei@xiaoshouyi.com>
2020-03-10 22:11:13 +0800

    up

--- offlinedev/jsmodule/static-injects/injectFiles/inject_global_script.js
+++ offlinedev/jsmodule/static-injects/injectFiles/inject_global_script.js
@@ -20,7 +20,11 @@
          let webbranch = rk_offlinedev.userConfig.webProjectInfo.branch;
          let debugmode = rk_offlinedev.userConfig.debug.mode;
          debugmode = debugmode?debugmode:'?'
-         div.innerHTML='<a style="color:#2fff56 !important;" href="http://localhost:666/offlinedev-http-console/" target="_blank">rk-offlinedev</a>,&nbsp;"'+debugmode+'",&nbsp;('+webbranch+')'
+         let debugcolor = (debugmode==='concat'?'color:#3497ff;':'')
+         div.innerHTML='<a style="color:#2fff56 !important;" href="http://localhost:666/offlinedev-http-console/" target="_blank">'+
+                        'rk-offlinedev'+
+                        '</a>,&nbsp;"<span style="'+debugcolor+'">'+debugmode+'</span>",&nbsp;('+
+                        webbranch+')'
          console.warn('[rk-offlinedev]seajs-text注入成功')
      };
      initRkOfflineDev()
