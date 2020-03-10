f9e95764700a40a8481a778d562629d3d21e83a2
zhanglei <zhanglei@xiaoshouyi.com>
2020-03-10 16:33:40 +0800

    up

--- offlinedev/jsmodule/static-proxy/updators/concat/hot_concat.js
+++ offlinedev/jsmodule/static-proxy/updators/concat/hot_concat.js
@@ -125,7 +125,7 @@ let loadHotFileConcatPlan = (staticfolder, sourcefolder)=>{
                                                                     {
                                                                         no_hot_url:true,
                                                                         depsPathIdUpdate:(depspathid)=>{//更新css的hot url，打包状态下，只需跟新define函数的就行。
-                                                                            if(!configUtil.getValue('debug.concatStaticCssRequests')) return;
+                                                                            if(!configUtil.getValue('debug.concatStaticCssRequests')) return depspathid;
                                                                             depspathid.forEach((pid, idx)=>{
                                                                                 let hotid = updateScript_CssUrl.changeToHotPath(fullfilepath, pid)
                                                                                 depspathid[idx] = hotid ? hotid : pid;
