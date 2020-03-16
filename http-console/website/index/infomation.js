eb8955d9297e62d4c5e4de6b2f6e68b91ef89efc
zhanglei <zhanglei@xiaoshouyi.com>
2020-03-16 15:57:46 +0800

    up

--- http-console/website/index/infomation.js
+++ http-console/website/index/infomation.js
@@ -28,7 +28,11 @@ let showInfomation = (result)=>{
         <tr><td align="right" valign="top">配置：</td><td>
             <span title="更改user-config里的: debug.mode" class="status_group">
             <span style="color:#d1d2d46e;">[</span><span class="${userConfig.debug && userConfig.debug.mode === 'source'?'status_is_on underline':'status_is_off '}">${"source"}</span><span style="color:#7c7e82;">/</span><span
-             class="${userConfig.debug && userConfig.debug.mode === 'concat'?'underline':'status_is_off '}" style="color:#00feff;">${"concat"}</span><span style="color:#d1d2d46e;">]</span>
+             class="${userConfig.debug && userConfig.debug.mode === 'concat'?'':'status_is_off '}" style="color:#00feff;"
+             >${"concat"}${
+                 (userConfig.debug.mode === 'concat' && userConfig.debug.concat.filesToExclude && userConfig.debug.concat.filesToExclude.length>0)?
+                 '<span class="status_warn_fill">'+userConfig.debug.concat.filesToExclude.length+' excludes</span>':''
+                }</span><span style="color:#d1d2d46e;">]</span>
             </span>
             <span title="更改user-config里的: httpConfig.http2" class="${userConfig.httpConfig && userConfig.httpConfig.http2?'status_is_on status_is_positive':'status_is_off status_is_neutral'}">h2</span>
             <span title="更改user-config里的: httpConfig.gzip" class="${userConfig.httpConfig && userConfig.httpConfig.gzip?'status_is_on status_is_positive':'status_is_off status_is_negative'}">gzip</span>
