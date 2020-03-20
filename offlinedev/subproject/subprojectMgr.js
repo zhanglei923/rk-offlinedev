507523d1bbd19c6891d3bda197221d250f4e9f9e
zhanglei <zhanglei@xiaoshouyi.com>
2020-03-21 01:27:08 +0800

    up

---
+++ offlinedev/subproject/subprojectMgr.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+let _ = require('lodash')
+var execSh = require("exec-sh");
+
+let eachProjects = (callback)=>{//所有子工程和主工程
+
+};
+
+module.exports = {
+    eachProjects
+};
