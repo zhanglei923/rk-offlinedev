77bc19d0c002ac35ad9fe307abcf35a4c8711c4d
zhanglei <zhanglei@xiaoshouyi.com>
2020-03-23 19:30:27 +0800

    up

--- http-console/website/index/multi_projects.js
+++ http-console/website/index/multi_projects.js
@@ -1,11 +1,14 @@
 let initMultiProjectEvents = ()=>{
     $('body').on('click', 'a[act="clone_sub_projects"]', function(){
         let btn = $(this);
+        if(btn.hasClass('is_working')) return;
         let project = btn.attr('project');
         let branch = btn.attr('branch');
+        beWorkingBtn(btn);
         if(confirm(`确认重新下载${project}工程么，当前文件会被清除。`)){
             cloneProject(project, branch, (status)=>{
                 alert('done, '+status)
+                unWorkingBtn(btn);
             })
         }
     })
---
+++ http-console/website/index/util.js
@@ -0,0 +1,12 @@
+let beWorkingBtn = (btn)=>{
+    let orignal_html = btn.html();
+    btn.attr('orignal_html', encodeURIComponent(orignal_html));
+    btn.addClass('is_working');
+    btn.html('working...');
+}
+let unWorkingBtn = (btn)=>{
+    let orignal_html = btn.attr('orignal_html');
+    orignal_html = decodeURIComponent(orignal_html);
+    btn.removeClass('is_working');
+    btn.html(orignal_html)
+}
