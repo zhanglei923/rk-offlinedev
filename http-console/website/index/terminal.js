56cc63c7a23c46a5ee284d6d097b459cfaece510
zhanglei <zhanglei@xiaoshouyi.com>
2020-03-26 06:15:41 +0800

    up

--- http-console/website/index/terminal.js
+++ http-console/website/index/terminal.js
@@ -1,3 +1,26 @@
 let openTerminal = (prjpath)=>{
     window.open('./pages/terminal/xterm.html?folder='+(prjpath))
 }
+let execShell = (prjpath, callback)=>{
+    if(typeof callback === 'undefined') callback = ()=>{};
+    $.ajax({
+        url: '/offlinedev/api/terminal/exec',
+        cache: false,
+        method: 'POST',
+        data: {
+            inputline: encodeURIComponent(inputline), 
+            prjpath
+        },
+        success: function( response ) {
+          let result = response.result;
+          if(result){
+            result = decodeURIComponent(result);
+            console.log(result);
+            callback(result);
+          }
+        },
+        error:function(ajaxObj,msg,err){
+            callback(false)
+        }
+    });
+}
