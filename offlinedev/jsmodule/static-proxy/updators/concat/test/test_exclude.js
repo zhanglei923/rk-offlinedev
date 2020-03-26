f98cd18ad72d1c06d03b752ba77e35da55e199c5
zhanglei <zhanglei@xiaoshouyi.com>
2020-03-26 16:08:55 +0800

    fix

--- offlinedev/jsmodule/static-proxy/updators/concat/test/test_exclude.js
+++ offlinedev/jsmodule/static-proxy/updators/concat/test/test_exclude.js
@@ -2,12 +2,14 @@ let exclude = require('../exclude')
 
 exclude.setSettings({
                         filesToExclude:[
-                        'platform',
+                        'platform/**',
                         'oa/**/*.js',
-                        'products/bi/**'
+                        'products/bi/**',
+                        "oldcrm/**/*.js"
                     ]})
 
 console.log(exclude.isExcludePathid('platform/oa/workreport/widget.js'))
 console.log(exclude.isExcludePathid('oa/workreport/widget.js'))
 console.log(exclude.isExcludePathid('oa/workreport/widget.tpl'))
 console.log(exclude.isExcludePathid('products/bi/chart/widget.tpl'))
+console.log(exclude.isExcludePathid('oldcrm/js/core/business-widgets.js'))
