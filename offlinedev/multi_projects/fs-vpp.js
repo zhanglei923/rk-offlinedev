e48db4070c7bc57e81ab40be0aefc409c7986d5b
zhanglei <zhanglei@xiaoshouyi.com>
2020-03-26 18:19:27 +0800

    up

--- offlinedev/multi_projects/fs-vpp.js
+++ offlinedev/multi_projects/fs-vpp.js
@@ -1,3 +1,9 @@
 //VPP = Virtual Path Protocal
 let fs = require('fs-extra');
 let pathutil = require('path')
+
+// setWebProjectPath();
+// getSourceList();//返回所有主子工程的source目录地址
+// eachSourceList();//返回所有主子工程的source目录地址
+// existSync(fullfilepath);
+// getRealFilePath(fullfilepath);//返回真正地址
