a722388cff9a42a2cd7e8dd7b60ef52033a59e6a
zhanglei <zhanglei@xiaoshouyi.com>
2020-03-28 15:02:05 +0800

    up

--- offlinedev/multi_projects/fs-vpp.js
+++ offlinedev/multi_projects/fs-vpp.js
@@ -2,6 +2,11 @@
 let fs = require('fs-extra');
 let pathutil = require('path')
 
+let vpp_sourcefolder;
+global.vpp_setSourceFolder = (sourcedir)=>{
+    vpp_sourcefolder = sourcedir;
+}
+
 // setWebProjectPath();
 // getSourceList();//返回所有主子工程的source目录地址
 // eachSourceList();//返回所有主子工程的source目录地址
