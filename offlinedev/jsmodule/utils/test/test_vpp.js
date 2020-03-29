708e3afcabefeca397b1a0473bf004957c7de556
zhanglei <zhanglei@xiaoshouyi.com>
2020-03-29 14:54:10 +0800

    up

--- offlinedev/jsmodule/utils/test/test_vpp.js
+++ offlinedev/jsmodule/utils/test/test_vpp.js
@@ -13,6 +13,7 @@ console.log('src:', vpp.getSourceDir(`/Users/zhanglei/workspaces/subprojects/xsy
 
 console.log('pid:', rk_getPathId(`/Users/zhanglei/workspaces/subprojects/apps-ingage-web/src/main/webapp/static/router.js`))
 console.log('pid:', rk_getPathId(`/Users/zhanglei/workspaces/subprojects/xsy-static-creekflow/static/source/core/rk.crm.js`))
+console.log('pid:', rk_getPathId(`/Users/zhanglei/workspaces/subprojects/xsy-static-creekflow/static/source/core//sth//static/source//rk.crm.js`))
 
 vpp.eachSourceFolders((f)=>{
     console.log(f);
