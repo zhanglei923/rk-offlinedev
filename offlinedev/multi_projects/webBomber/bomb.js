6e31011796e9bfb17e7df49a49d7c578f2f8cca5
zhanglei <zhanglei@xiaoshouyi.com>
2020-04-09 17:52:38 +0800

    up

--- offlinedev/multi_projects/webBomber/bomb.js
+++ offlinedev/multi_projects/webBomber/bomb.js
@@ -24,7 +24,7 @@ let plan = [
 let cloneProject = (dir, pname, branch, callback)=>{
     makeDir.sync(new_workspace);
     let init_static_cmd = [
-        `cd ${new_workspace} && rm -rf ${pname}`,
+        `cd ${dir} && rm -rf ${pname}`,
         `git clone http://gerrit.ingageapp.com/${pname}`,
         `cd ${pname}`,
         `git pull`,
@@ -33,22 +33,51 @@ let cloneProject = (dir, pname, branch, callback)=>{
     execSh(`${init_static_cmd.join(' && ')}`, true, function(err, stdout, stderr){
         callback();
     });
-}
+};
 cloneProject(new_workspace, 'xsy-static', 'master', ()=>{
-    console.log('done')
+    console.log('init done')
+    let staticdir = `${new_workspace}/xsy-static`;
+    let cp_static_cmd = [
+        `echo "copy /static"`,
+        `cd ${staticdir}`,
+        `mkdir static && cd static`,
+        `cp -r ${static_path}/* ./`
+    ];
+    execSh(`${cp_static_cmd.join(' && ')}`, true, function(err, stdout, stderr){
+        console.log('cp done')
+        doPlan(plan);
+    });
+
 })
 
-plan.forEach((msg)=>{
-    let arr = msg.split(',');
+let doPlan = (theplan)=>{
+    if(theplan.length === 0) {
+        return;
+    }
+    let oneplan = theplan.shift();
+
+    let arr = oneplan.split(',');
     let folder = arr[0];
     let projectname = arr[1];
     let projectbranch = arr[2]?arr[2]:'master';
 
     let fullfolder = pathutil.resolve(static_path, folder);
+    console.log(fs.existsSync(fullfolder), fullfolder);
 
-    console.log(fs.existsSync(fullfolder), fullfolder)
+    cloneProject(new_workspace, projectname, projectbranch, ()=>{
+        let staticdir = `${new_workspace}/xsy-static/static`
+        let split_static_cmd = [
+            `echo "split /static"`,
+            `cd ${staticdir}`,
+            `mv ${folder} ${new_workspace}/${projectname}/static/source`
+        ];
+        execSh(`${split_static_cmd.join(' && ')}`, true, function(err, stdout, stderr){
+            console.log('cp done')
+        });
 
 
+        doPlan(theplan);//执行下一个
+    })
+}
 
-
-})
+//doPlan(plan);
